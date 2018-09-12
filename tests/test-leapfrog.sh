3f4e317ac80a10a268d7fa2c0066fe70b54b297a
Antony Messerli <amesserl@rackspace.com>
2018-09-12 13:36:48 -0500

    Disable MAAS verify in upgrades gating
    Verify is unstable and breaks often.
    Disabling as it doesn't provide a lot of value
    in gate testing.

--- tests/test-leapfrog.sh
+++ tests/test-leapfrog.sh
@@ -37,10 +37,10 @@ for FILENAME in ${VAULT_ENCRYPTED_FILES}; do
   fi
 done
 
-if [ "${RE_JOB_SERIES}" == "kilo" ]; then
-  export UPGRADE_ELASTICSEARCH=no
-  export CONTAINERS_TO_DESTROY='all_containers:!galera_all:!neutron_agent:!ceph_all:!rsyslog_all'
-fi
+
+# disable elasticsearch upgrade by default for gating
+export UPGRADE_ELASTICSEARCH=no
+export CONTAINERS_TO_DESTROY='all_containers:!galera_all:!neutron_agent:!ceph_all:!rsyslog_all'
 
 # export the term to avoid unknown: I need something more specific error in jenkins
 export TERM=linux
